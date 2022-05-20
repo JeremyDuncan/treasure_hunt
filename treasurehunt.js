b6379c818fe4912c8cfe9aaf1d856275d0e8e49f
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-20 08:41:38 -0400

    updated formatting

--- treasurehunt.js
+++ treasurehunt.js
@@ -1,44 +1,42 @@
-let chest = 0
-let bomb = 0
-let gameOver = false
-let youWin = false
-let clickedOn = []
+let chest = 0;
+let bomb = 0;
+let gameOver = false;
+let youWin = false;
+let clickedOn = [];
 
-onload = function() {
-  clickedOn = []
+onload = function () {
+  clickedOn = [];
 
   while (chest == bomb) {
-    chest = Math.floor(Math.random() * 24) 
-    bomb = Math.floor(Math.random() * 24)
+    chest = Math.floor(Math.random() * 24);
+    bomb = Math.floor(Math.random() * 24);
   }
-  document.getElementById("counter").innerHTML = 10 - clickCounter
-}
+  document.getElementById("counter").innerHTML = 10 - clickCounter;
+};
 
-let clickCounter = 0
+let clickCounter = 0;
 
 treasure = (num) => {
   // If user has not clicked on selection before,
   // save selection, then continue function
-  if (clickedOn.includes(num) ===  false) {
-    clickedOn.push(num)
+  if (clickedOn.includes(num) === false) {
+    clickedOn.push(num);
 
     if (num === chest) {
-      document.getElementById(num).innerHTML = "&#x1f308"
-      youWin = true
-    }else if (num === bomb) {
-      document.getElementById(num).innerHTML = "&#x2620"
-      gameOver = true
+      document.getElementById(num).innerHTML = "&#x1f308";
+      youWin = true;
+    } else if (num === bomb) {
+      document.getElementById(num).innerHTML = "&#x2620";
+      gameOver = true;
     } else {
-      document.getElementById(num).innerHTML = "&#x1f332"
+      document.getElementById(num).innerHTML = "&#x1f332";
     }
 
-    clickCounter++
+    clickCounter++;
     if (clickCounter > 9) {
-      gameOver = true
-      alert("Game over!")
+      gameOver = true;
+      alert("Game over!");
     }
   }
-  document.getElementById("counter").innerHTML = 10 - clickCounter
-
-}
-
+  document.getElementById("counter").innerHTML = 10 - clickCounter;
+};
