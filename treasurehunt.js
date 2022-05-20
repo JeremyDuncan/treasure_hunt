07723f421ee38ec9d3328df10b24a7718700607d
Jeremy Duncan <jeremy.duncan1984@gmail.com>
2022-05-19 22:21:52 -0400

    added new logic to treasure function

--- treasurehunt.js
+++ treasurehunt.js
@@ -3,9 +3,9 @@ bomb = 0
 
 treasure = (num) => {
   if (num === chest) {
-    alert("treasure " + num)
+    document.getElementById(num).innerHTML = "&#x1f308"
   }else if (num === bomb) {
-    alert("Bomb " + num)
+    document.getElementById(num).innerHTML = "&#x2620 "
   } else {
     document.getElementById(num).innerHTML = "&#x1f332"
   }
